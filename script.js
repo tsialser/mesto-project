efcc8b66d1613d1f3e3c459afe448d12159b52bc
Aleksey Tsimbal <lehatsimbal@yandex.ru>
2022-02-21 16:46:53 +0700

    реализовано редактирование профиля

--- script.js
+++ script.js
@@ -1,44 +1,73 @@
-//Находим форму в DOM
-const popupEditProfile = document.querySelector(".popup_js-edit-profile");
-const popupAddCard = document.querySelector(".popup_js-add-card");
-const addProfile = document.querySelector(".profile__edit-button");
-const addCard = document.querySelector(".profile__add-button");
-const closeProfile = document.querySelector(".js-close-profile");
-const closeCard = document.querySelector(".js-close-card");
-const editSave = document.querySelector(".popup_edit-save");
-const addSave = document.querySelector(".popup_add-save");
+const editButton = document.querySelector(".profile__edit-button");
+const closeButton = document.querySelector(".popup__close-button");
+const popup = document.querySelector(".popup");
+const popupProfile = document.querySelector('.popup__profile');
+const popupEditForm = document.getElementById('edit-profile');
 
-/* Обработчик «отправки» формы
+const profileName = document.getElementById('name');
+const profileJob = document.getElementById('job');
+const nameInput = document.querySelector('.profile__title');
+const jobInput = document.querySelector('.profile__subtitle');
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+  ];
+  
+
+
+// Функции открытия-закрытия самого модального окна
+function openPopup() {
+  popup.classList.add("popup_opened");
+}
+
+function closePopup() {
+  popup.classList.remove("popup_opened");
+}
+
+// Открытие-Закрытие формы редактирования профиля 
+editButton.addEventListener("click", () => {
+  openPopup(popupProfile);
+  formValue();
+});
+
+closeButton.addEventListener("click", () => closePopup(popupProfile));
+
+// Заполнение формы значениями, указанными на странице
+function formValue() {
+  profileName.value = nameInput.textContent;
+  profileJob.value = jobInput.textContent;
+}
+
+// Редактирование информации о себе
 function formSubmitHandler(evt) {
-    evt.preventDefault();
-    const popup = document.querySelector('.popup');
-    popup.classList.add('popup_opened');
+  evt.preventDefault();
+  nameInput.textContent = profileName.value;
+  jobInput.textContent = profileJob.value;
+  closePopup(popupProfile);
+}
+popupEditForm.addEventListener('submit', formSubmitHandler);
 
-}*/
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-addProfile.addEventListener("click", function () {
-  popupEditProfile.classList.add("popup_opened");
-});
-
-addCard.addEventListener("click", function () {
-  popupAddCard.classList.add("popup_opened");
-});
-
-//Закрытие модального окна
-closeProfile.addEventListener("click", function () {
-  popupEditProfile.classList.remove("popup_opened");
-});
-
-editSave.addEventListener("click", function () {
-  popupEditProfile.classList.remove("popup_opened");
-});
-
-closeCard.addEventListener("click", function () {
-  popupAddCard.classList.remove("popup_opened");
-});
-
-addSave.addEventListener("click", function () {
-  popupAddCard.classList.remove("popup_opened");
-});
