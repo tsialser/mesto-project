4b37e9397ff4be8b421913500781f7b673c5ba61
Aleksey Tsimbal <lehatsimbal@yandex.ru>
2022-02-25 12:00:53 +0300

    Добавлено открытие карточек

--- script.js
+++ script.js
@@ -6,43 +6,43 @@ const closeButtonProfile = document.querySelector("#close-button-profile");
 const addCardButton = document.querySelector(".profile__add-button");
 const closeCardButton = document.querySelector("#close-button-cards");
 const formElement = document.querySelector("#form-profile");
-const formCards = document.querySelector('#form-cards');
+const formCards = document.querySelector("#form-cards");
 const nameInput = formElement.querySelector("#name");
 const jobInput = formElement.querySelector("#job");
-const cardTemplate = document.querySelector('#card-template').content;
+const cardTemplate = document.querySelector("#card-template").content;
 
 const initialCards = [
   {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    name: "Архыз",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
   },
   {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    name: "Челябинская область",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
   },
   {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    name: "Иваново",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
   },
   {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    name: "Камчатка",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
   },
   {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    name: "Холмогорский район",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
   },
   {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-  ];
+    name: "Байкал",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
+];
 
 //---------------------------------------------------Работа модальных окон------------------------------------------------
 
 // Функции открытия-закрытия самого модального окна
 function openPopup(popupElement) {
- popupElement.classList.add("popup_opened");
+  popupElement.classList.add("popup_opened");
 }
 
 function closePopup(popupElement) {
@@ -72,7 +72,6 @@ function formSubmitHandler(evt) {
   let profileTitle = document.querySelector(".profile__title");
   let profileSubtitle = document.querySelector(".profile__subtitle");
 
-
   profileTitle.textContent = nameInput.value;
   profileSubtitle.textContent = jobInput.value;
 
@@ -84,25 +83,41 @@ formElement.addEventListener("submit", formSubmitHandler);
 
 //-----------------------------------------------Форма добавления карточки------------------------------------------------
 
-
 const main = document.querySelector(".main");
 const elementsContainer = main.querySelector(".elements");
 const element = elementsContainer.querySelectorAll(".element");
 
 // Создание карточки
 function addCard(titleValue, linkValue) {
-  
-  const cardElement = cardTemplate.querySelector('.element').cloneNode(true);
+  const cardElement = cardTemplate.querySelector(".element").cloneNode(true);
 
-  cardElement.querySelector('.element__image').src = linkValue;
-  cardElement.querySelector('.element__image').alt = titleValue;
-  cardElement.querySelector('.element__title').textContent = titleValue;
-  cardElement.querySelector('.element__button').addEventListener('click', function (evt) {
-    evt.target.classList.toggle('element__button_active');
-  })
-  cardElement.querySelector('.element__delete-button').addEventListener('click', handleClick);
+  cardElement.querySelector(".element__image").src = linkValue;
+  cardElement.querySelector(".element__image").alt = titleValue;
+  cardElement.querySelector(".element__title").textContent = titleValue;
+
+  //Лайк
+  cardElement
+    .querySelector(".element__button")
+    .addEventListener("click", function (evt) {
+      evt.target.classList.toggle("element__button_active");
+    });
+
+  //Удаление
+  cardElement
+    .querySelector(".element__delete-button")
+    .addEventListener("click", handleClick);
+
+  //Открытие изображения для созданной карточки
+  cardElement
+    .querySelector(".element__image")
+    .addEventListener("click", (evt) => {
+      evt.preventDefault();
+      openImg(linkValue, titleValue);
+    });
 
   elementsContainer.prepend(cardElement);
+
+  return cardElement;
 }
 
 // Обработчик "отправки" формы добавления карточки в контейнер
@@ -120,53 +135,60 @@ function submitCardButton(evt) {
   closePopup(popupCards);
 }
 // Прикрепляем обработчик к форме
-formCards.addEventListener('submit', submitCardButton);
+formCards.addEventListener("submit", submitCardButton);
 
 //------------------------------------------------------------Шесть карточек из "коробки"---------------------------------------------------
 
 initialCards.forEach(function (element) {
   const cardElement = cardTemplate.cloneNode(true);
 
-  cardElement.querySelector('.element__image').src = element.link;
-  cardElement.querySelector('.element__image').textContent = element.name;
-  cardElement.querySelector('.element__title').textContent = element.name;
-  cardElement.querySelector('.element__button').addEventListener('click', function (evt) {
-    evt.target.classList.toggle('element__button_active');
-  })
+  cardElement.querySelector(".element__image").src = element.link;
+  cardElement.querySelector(".element__image").alt = element.name;
+  cardElement.querySelector(".element__title").textContent = element.name;
+  cardElement
+    .querySelector(".element__button")
+    .addEventListener("click", function (evt) {
+      evt.target.classList.toggle("element__button_active");
+    });
+
+  // Открытие попапа с изображением
+  cardElement
+    .querySelector(".element__image")
+    .addEventListener("click", (evt) => {
+      evt.preventDefault();
+      openImg(element.link, element.name);
+    });
 
   elementsContainer.append(cardElement);
-})
+});
 
 //------------------------------------------------------------Удаление карточки---------------------------------------------------
 
 // Выбираем кнопку удаления
-const deleteButton = document.querySelectorAll('.element__delete-button');
+const deleteButton = document.querySelectorAll(".element__delete-button");
 
 // Функция удаления карточки
 handleClick = (evt) => {
-  evt.target.closest('.element').remove();
-}
+  evt.target.closest(".element").remove();
+};
 
 // Добавляем обработчик
-deleteButton.forEach(item => item.addEventListener('click', handleClick));
+deleteButton.forEach((item) => item.addEventListener("click", handleClick));
 
 //------------------------------------------------------------Открытие попапа с картинкой--------------------------------------------------
 
-//Находиим изображение
-const imgElement = document.querySelectorAll('.element__image');
-const popupImage = document.querySelector('.popup__image');
-const closeButtonImage = document.querySelector('#close-button-img');
+const openImgPopup = document.querySelector(".popup__image");
+const closeImgPopup = document.querySelector("#close-button-img");
 
-//Открытие-Закрытия попапа с изображением по клику
-for(let i = 0; i < imgElement.length; i++) {
-  let img = imgElement[i];
+// Закрытие попапа с изобраением
+closeImgPopup.addEventListener("click", () => {
+  closePopup(openImgPopup);
+});
 
-  img.addEventListener('click', () => {
-    openPopup(popupImage);
-  })};
-
-closeButtonImage.addEventListener('click', () => {
-  closePopup(popupImage);
-})
-
-//
+// Функция открытия Изображения
+function openImg(linkImg, titleImg) {
+  openPopup(openImgPopup);
+  openImgPopup.querySelector(".popup__img").src = linkImg;
+  openImgPopup.querySelector(".popup__img").alt = titleImg;
+  openImgPopup.querySelector(".popup__title-img").textContent = titleImg;
+}
