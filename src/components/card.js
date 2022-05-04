ef42262809857c85907b12f47643e5d82e004937
Aleksey Tsimbal <lehatsimbal@yandex.ru>
2022-05-04 05:04:32 +0300

    Добавление новой карточки

--- src/components/card.js
+++ src/components/card.js
@@ -1,51 +1,46 @@
 import {
   title,
   link,
-  formCards,
   popupCards,
   cardTemplate,
-  initialCards,
   elementsContainer,
   openImgPopup,
   popupImg,
   popupImgTitle
 } from "./constants.js";
 import { openPopup, closePopup } from "./modal.js";
-import { getCards, addNewCard } from "./api.js";
+import { addNewCard } from "./api.js";
 
 export function handleCardSubmit(evt) {
   evt.preventDefault();
-/*
-  addCard(
-    elementsContainer,
-    createCard(formCards.title.value, formCards.link.value)
-  );*/
-  addNewCard({
-    name: title.value,
-    link: link.value
-  })
-    .then((data) => {
-      const card = createCard(
-        data.name,
-        data.link
-      )
-      closePopup(popupCards);
+
+    addNewCard({
+      link: link.value,
+      name: title.value,
+      
     })
-}
-
-
-
+      .then((data) => {
+        const card = createCard(
+          data.link,
+          data.name
+        );
+        closePopup(popupCards);
+        elementsContainer.prepend(card);
+      })
+      .catch((err) => {
+        console.error(err);
+      })
+    }
 
 // Создание карточки
-export function createCard(name, link) {
+export function createCard(link, title) {
   const cardElement = cardTemplate.querySelector(".element").cloneNode(true);
   const cardImage = cardElement.querySelector('.element__image');
   const cardTitle = cardElement.querySelector(".element__title");
 
-  cardImage.src = link.value;
-  cardImage.textContent = name.value;
-  cardTitle.textContent = name.value;
-
+  cardImage.src = link;
+  cardImage.alt = title;
+  cardTitle.textContent = title;
 
   //Лайк
   cardElement
@@ -61,27 +56,16 @@ export function createCard(name, link) {
 
   //При клике на вновь созданную карточку открываем изображение
   cardImage.addEventListener("click", () => {
-    popupImage(link, name);
+    popupImage(link, title);
   });
 
   return cardElement;
 }
 
-// Добавление карточек
-export function addCard(container, cardElement) {
-  container.prepend(cardElement);
-}
-
 // Функция удаления карточки
-export function handleDeleteClick(evt) {
+function handleDeleteClick(evt) {
   evt.target.closest(".element").remove();
 }
-/*
-// 6 карточек из коробки
-initialCards.forEach((card) => {
-  addCard(elementsContainer, createCard(card.name, card.link));
-});
-*/
 
 // Функция открытия Изображения
 function popupImage(linkImg, titleImg) {
