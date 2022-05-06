272134de208c434eb28164457c9e0dd094f65dd7
Aleksey Tsimbal <lehatsimbal@yandex.ru>
2022-05-06 04:13:46 +0300

    Постановка и снятие лайка

--- src/components/api.js
+++ src/components/api.js
@@ -1,58 +1,66 @@
 const config = {
-    baseUrl: "https://nomoreparties.co/v1/plus-cohort-9",
-    headers: {
-        authorization: "49010d44-315f-475d-8895-b34a2d4f4848",
-        "Content-Type": "application/json",
-    },
+  baseUrl: "https://nomoreparties.co/v1/plus-cohort-9",
+  headers: {
+    authorization: "49010d44-315f-475d-8895-b34a2d4f4848",
+    "Content-Type": "application/json",
+  },
 };
 
 function responseCheck(res) {
-    if (res.ok) {
-        return res.json();
-    } else {
-        return Promise.reject(`Ошибка: ${res.status}`);
-    }
+  if (res.ok) {
+    return res.json();
+  } else {
+    return Promise.reject(`Ошибка: ${res.status}`);
+  }
 }
 
-export const editProfile = newProfile => {
-    return fetch(`${config.baseUrl}/users/me`, {
-        method: 'PATCH',
-        headers: config.headers,
-        body: JSON.stringify(newProfile)
-    })
-    .then(responseCheck);
+export const editProfile = (newProfile) => {
+  return fetch(`${config.baseUrl}/users/me`, {
+    method: "PATCH",
+    headers: config.headers,
+    body: JSON.stringify(newProfile),
+  }).then(responseCheck);
 };
 
-export const addNewCard = cardData => {
-    return fetch(`${config.baseUrl}/cards`, {
-        method: 'POST',
-        headers: config.headers,
-        body: JSON.stringify(cardData)
-    })
-    .then(responseCheck);
-}
+export const addNewCard = (cardData) => {
+  return fetch(`${config.baseUrl}/cards`, {
+    method: "POST",
+    headers: config.headers,
+    body: JSON.stringify(cardData),
+  }).then(responseCheck);
+};
 
 export const getUserInformation = () => {
-    return fetch(`${config.baseUrl}/users/me`, {
-        method: 'GET',
-        headers: config.headers
-    })
-    .then(responseCheck);
-}
-
+  return fetch(`${config.baseUrl}/users/me`, {
+    method: "GET",
+    headers: config.headers,
+  }).then(responseCheck);
+};
 
 export const getCards = () => {
-    return fetch(`${config.baseUrl}/cards`, {
-        method: 'GET',
-        headers: config.headers
-    })
-    .then(responseCheck);
-}
+  return fetch(`${config.baseUrl}/cards`, {
+    method: "GET",
+    headers: config.headers,
+  }).then(responseCheck);
+};
 
 export const deleteCard = (cardID) => {
-    return fetch(`${config.baseUrl}/cards/${cardID}`, {
-        method: 'DELETE',
-        headers: config.headers,
-    })
-    .then(responseCheck);
-}
+  return fetch(`${config.baseUrl}/cards/${cardID}`, {
+    method: "DELETE",
+    headers: config.headers,
+  }).then(responseCheck);
+};
+
+export const likeCard = (cardID) => {
+  return fetch(`${config.baseUrl}/cards/likes/${cardID}`, {
+    method: "PUT",
+    headers: config.headers,
+  }).then(responseCheck);
+};
+
+export const unlikeCard = (cardID) => {
+  return fetch(`${config.baseUrl}/cards/likes/${cardID}`, {
+    method: "DELETE",
+    headers: config.headers,
+  }).then(responseCheck);
+};
